b9d577d10b9530b4f6429b91d9ef70a13f682d24
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-11-01 10:00:24 +0100

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -23,20 +23,23 @@ contract ZombieFeeding is ZombieFactory {
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-
+    // Add an if statement here
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))){
+      newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-   
-    feedAndMultiply(_zombieId, kittyDna);
+    // And modify function call here:
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
