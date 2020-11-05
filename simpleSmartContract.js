a0250394fcf5e1d7620d35811cf02be3d71489e0
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-11-05 09:45:06 +0100

    test for smart contract

---
+++ simpleSmartContract.js
@@ -0,0 +1,11 @@
+const SimpleSmartContract = artifacts.require('SimpleSmartContract');
+
+contract('SimpleStorageContract', () => {
+
+it('Should deploy smart contract properly', async() => {
+
+const simpleSmartContract = await SimpleSmartContract.deployed();
+console.log(simpleSmartContract.address);
+assert(simpleSmartContract.address !== '');
+ });
+});
